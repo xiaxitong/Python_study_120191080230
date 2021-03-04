8870360404f1fa53a1dbf158a7b54eeea072e197
wenhongsu <1654682472@qq.com>
2021-03-04 16:11:36 +0800

    2021.3.4

---
+++ exercises/_str_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:_str_string.py
+# author:16546
+# datetime:2021/3/4 16:04
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+if __name__=="__main__":
+    str = 'abcdef'
+    print(str[-1],str[-2],str[-3],str[-4],str[-5],str[-6])
