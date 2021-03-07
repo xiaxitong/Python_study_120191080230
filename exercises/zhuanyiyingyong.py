13d30ab70f6a3f8d7df8ae28e867dedfc5904694
wenhongsu <1654682472@qq.com>
2021-03-07 12:14:57 +0800

    2021.3.7

---
+++ exercises/zhuanyiyingyong.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:zhuanyiyingyong.py
+# author:16546
+# datetime:2021/3/7 12:10
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+print('亲爱的XXX：\n\t请点击连接激活用户：激活用户')
