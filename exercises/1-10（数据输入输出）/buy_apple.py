975a9618d157b3d13de2ae1df01e48bc861decb1
wenhongsu <1654682472@qq.com>
2021-03-28 09:58:32 +0800

    2021.3.28

---
+++ exercises/1-10（数据输入输出）/buy_apple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:buy_apple.py
+# author:16546
+# datetime:2021/3/28 9:10
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+if __name__=="__main__":
+    weight = int(input("输入重量："))
+    price = int(input("输入价格："))
+    print(f'总价为：{weight*price}')
---
+++ exercises/1-10（数据输入输出）/str_to_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:str_to_tuple.py
+# author:16546
+# datetime:2021/3/28 9:05
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+if __name__=="__main__":
+    x = map(int,input().split( ))
+    tup = tuple(x)
+    print(tup)
